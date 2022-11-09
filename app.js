b4017e886d4f4b37a1153402971082ed3779c9a7
raviniatagay <raviniatagay@yahoo.com>
2022-11-08 18:28:45 -0600

    Simple Node Example

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-// const rect = {
-//     perimeter: (x, y) => 2 * (x + y),
-//     area: (x, y) => x * y
-// };
-const rect = require('./rectangle');
+const rect = {
+    perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+// const rect = require('./rectangle');
 
-// function solveRect(l, w) {
-//     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
-
-//     if (l <= 0 || w <= 0) {
-//         console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-//     } else {
-//         console.log(`Area of rectangle: ${rect.area(l, w)}`);
-//         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-//     }
-// }
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-    rect(l, w, (err, rectangle) => {
-        if (err) {
-            console.log('ERROR:', err.message);
-        } else {
-            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
-            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
-        }
-    });
-    console.log('This statement is logged after the call to rect()');
+    if (l <= 0 || w <= 0) {
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
+    } else {
+        console.log(`Area of rectangle: ${rect.area(l, w)}`);
+        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
+    }
 }
+// function solveRect(l, w) {
+//     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+//     rect(l, w, (err, rectangle) => {
+//         if (err) {
+//             console.log('ERROR:', err.message);
+//         } else {
+//             console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+//             console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+//         }
+//     });
+//     console.log('This statement is logged after the call to rect()');
+// }
 
 solveRect(2, 4);
 solveRect(3, 5);
